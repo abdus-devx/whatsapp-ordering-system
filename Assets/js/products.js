09e9ef58f24fe25b3d6c4432e93d15a3e6484489
abdus-devx <misterabdus.com>
2026-09-08 20:47:45 +0700

    refactor:change render produk from html to js

---
+++ Assets/js/products.js
@@ -0,0 +1,49 @@
+const products = [
+  {
+    id: 1,
+    name: "Kapsul Sari Ikan Gabus",
+    category: "kapsul",
+    price: 170000,
+    image: "./Assets/images/sig_PASCAOPERASI.avif",
+    description:
+      "Cocok untuk: Pemulihan luka pasca-operasi dan melahirkan, memperbaiki masalah gizi buruk dan meningkatkan stamina.",
+    bestseller: false,
+    promo: false,
+  },
+
+  {
+    id: 2,
+    name: "Madu Arjuna (Pria Dewasa)",
+    category: "madu",
+    price: 150000,
+    image: "./Assets/images/arjuna_madu.avif",
+    description:
+      "Membantu meningkatkan vitalitas dan stamina pria dewasa.",
+    bestseller: false,
+    promo: false,
+  },
+
+  {
+    id: 3,
+    name: "Kapsul Toper (30 Kapsul)",
+    category: "kapsul",
+    price: 200000,
+    image: "./Assets/images/kapsul toper.avif",
+    description:
+      "Membantu meredakan mual, tonik perut, Gerd dan gangguan lambung lainnya.",
+    bestseller: false,
+    promo: false,
+  },
+
+  {
+    id: 4,
+    name: "Madu Ginseng (330g)",
+    category: "madu",
+    price: 190000,
+    image: "./Assets/images/ginseng_madu.avif",
+    description:
+      "Membantu meningkatkan antibodi, menjaga stamina tetap prima.",
+    bestseller: false,
+    promo: false,
+  },
+];
